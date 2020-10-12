93998dcd979e253d522e68575aec5b477586bda9
taisuke.fujimoto <taisuke.fujimoto@phoneappli.net>
2020-10-12 11:39:07 +0900

    add failing test

--- spring-webapp/src/test/kotlin/my/webapp/SampleTest.kt
+++ spring-webapp/src/test/kotlin/my/webapp/SampleTest.kt
@@ -1,9 +1,23 @@
 package my.webapp
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeExactly
 
 class SampleTest : StringSpec({
     "try println" {
         println("[try println] test executed.")
+    }
+
+    "this test fails." {
+        val one = 1
+        val two = 2
+
+        one shouldBeExactly two
+    }
+
+    "this test succeeds" {
+        val one = 1
+
+        one shouldBeExactly one
     }
 })
